0a5b8ff05c4717cb42e5232f1cfc423bee78d2b8
gitAlbertadine <albertadine760@gmail.com>
2019-04-26 00:51:58 +0100

    hbnnbfv

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>a si said src/App.js</code> and save to reload.
+          Edit <code>a src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
